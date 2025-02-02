0ce7985583d83b6eafc7a4523d81ef51dac886f4
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-02-02 19:35:50 +0530

    change name of backend url

--- notepad/frontEnd/src/constant/constants.js
+++ notepad/frontEnd/src/constant/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://google-keep-clone-liard-chi.vercel.app/user';
-export const API_NOTE = 'https://google-keep-clone-liard-chi.vercel.app/keepnote'
-export const API_Label = 'https://google-keep-clone-liard-chi.vercel.app/label'
+export const API_URL = 'https://google-keep-clone-one-pearl.vercel.app/user';
+export const API_NOTE = 'https://google-keep-clone-one-pearl.vercel.app/keepnote'
+export const API_Label = 'https://google-keep-clone-one-pearl.vercel.app/label'
